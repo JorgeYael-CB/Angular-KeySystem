626c773a034595a7d8dbd20f1adb70909618e504
JorgeYael-CB <jorge.yael333@gmail.com>
2024-09-15 12:41:34 -0600

    First commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { KeyComponent } from './pages/key/key.component';
+
+
+
+export const routes: Routes = [
+  {path: "about", component: AboutComponent},
+  {path: "key", component: KeyComponent},
+];
